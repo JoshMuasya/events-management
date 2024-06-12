16f068482b9cf6ebc3392c1430f191362cc8a8cb
JoshMuasya <muasyajoshua07@gmail.com>
2024-06-12 13:26:04 +0300

    Updated Navbar

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,9 +1,44 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Menu = () => {
   return (
-    <div className='text-card'>
-      Menu
+    <div className='text-card font-medium text-lg w-full flex flex-row justify-around items-center align-middle'>
+      {/* Link 1 */}
+      <div>
+        <Link
+          href=''
+        >
+          Home
+        </Link>
+      </div>
+
+      {/* Link 2 */}
+      <div>
+        <Link
+          href=''
+        >
+          About Us
+        </Link>
+      </div>
+
+      {/* Link 3 */}
+      <div>
+        <Link
+          href=''
+        >
+          Services
+        </Link>
+      </div>
+
+      {/* Link 4 */}
+      <div>
+        <Link
+          href=''
+        >
+          Portfolio
+        </Link>
+      </div>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,11 @@ import Menu from './Menu'
 
 const Navbar = () => {
     return (
-        <div className='fixed top-10 left-20 z-20 flex flex-row justify-around items-center align-middle w-full'>
+        <nav className='fixed top-10 left-0 z-20 flex flex-row justify-around items-center align-middle w-full'>
             {/* Logo */}
-            <div className='flex flex-row justify-center items-center align-middle'>
+            <Link
+                href=''
+                className='flex flex-row justify-center items-center align-middle'>
                 <div>
                     <Image
                         src='/nav-logo-dark.svg'
@@ -22,21 +24,53 @@ const Navbar = () => {
                 <h1 className='text-3xl text-white pl-5'>
                     Digimatic
                 </h1>
-            </div>
+            </Link>
 
             {/* Menu */}
-            <div>
-                <Menu />
+            {/* Link 1 */}
+            <div className='text-card hover:text-primary hover:font-bold font-medium text-lg hover:text-xl transition duration-500 ease-in-out'>
+                <Link
+                    href=''
+                >
+                    Home
+                </Link>
+            </div>
+
+            {/* Link 2 */}
+            <div className='text-card hover:text-primary hover:font-bold font-medium text-lg hover:text-xl transition duration-500 ease-in-out'>
+                <Link
+                    href=''
+                >
+                    About Us
+                </Link>
+            </div>
+
+            {/* Link 3 */}
+            <div className='text-card hover:text-primary hover:font-bold font-medium text-lg hover:text-xl transition duration-500 ease-in-out'>
+                <Link
+                    href=''
+                >
+                    Services
+                </Link>
+            </div>
+
+            {/* Link 4 */}
+            <div className='text-card hover:text-primary hover:font-bold font-medium text-lg hover:text-xl transition duration-500 ease-in-out'>
+                <Link
+                    href=''
+                >
+                    Portfolio
+                </Link>
             </div>
 
             {/* Contact Button */}
             <div>
-                <Link 
-                href=''
-                className={buttonVariants({ variant: "default" })}>Contact Us
+                <Link
+                    href=''
+                    className={buttonVariants({ variant: "default" })}>Contact Us
                 </Link>
             </div>
-        </div>
+        </nav>
     )
 }
 
